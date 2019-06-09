82b91e0232d71ce6419e9cf0e3a7facd6ce9a1f2
Davi-IF <p.davi.nunes@gmail.com>
2019-06-09 15:41:18 -0300

    Construtor na classe Bicycle

--- passo1/main.dart
+++ passo1/main.dart
@@ -3,9 +3,15 @@ class Bicycle {
 	int cadence;
 	int speed;
 	int gear;
+
+	//construtor
+	Bicycle(this.cadence, this.speed, this.gear);
 }
 
 
 void main() {
+
+	var bike = Bicycle(2, 0, 1);
+	print(bike);
 	
 }
