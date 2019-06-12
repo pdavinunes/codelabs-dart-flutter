f30b5d33d7db865ac44aeeb2340b48a23cc62c80
Davi-IF <p.davi.nunes@gmail.com>
2019-06-12 03:07:38 -0300

    Código funcional na classe Scream

--- passo1/Scream.dart
+++ passo1/Scream.dart
@@ -4,7 +4,5 @@ String scream(int tam) => 'A${'a' * tam}h!';
 void main(){
 	final valores = [2, 3, 5, 10, 60];
 
-	for(var tam in valores){
-		print(scream(tam));
-	}
+	valores.skip(1).take(3).map(scream).forEach(print);
 }
